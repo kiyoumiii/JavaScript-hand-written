57049cd4f465e156b2e9ea68ced03defa413df49
kiyoumiii <1749380324@qq.com>
2025-03-07 15:19:33 +0800

    柯里化

---
+++ 函数柯里化.js
@@ -0,0 +1,20 @@
+// 将多参数函数转换为一系列单参数函数，直到收集到足够的参数后才执行原始函数。
+// 返回一个柯里化后的函数
+// 检查当前收集到的参数数量是否已经满足原始函数的参数数量要求，如果足够就调用原始函数，不足1就继续收集参数
+
+function curry (fn) {
+    if (typeof fn !== 'function') {
+        return new Error('curry() need a function');
+    }
+
+    return function curried(...args) {
+        if (args.length >= fn.length) { // 参数的数量收集足够
+            return fn.apply(this, args);
+        }
+        else {
+            return function(...args2) {
+                return curried.apply(this, args.concat(args2)); // 继续收集
+            }
+        }
+    }
+}
